7c41b288c616eb862c80c1ae9310f826074a32c9
Noah Marrano <nmarrano@cedarville.edu>
2025-11-26 23:08:34 -0600

    1st draft: add() and removeRoot() completed with full implementation

--- src/project7/ArrayHeap.java
+++ src/project7/ArrayHeap.java
@@ -23,16 +23,80 @@ public class ArrayHeap extends ArrayBinaryTree implements Heap {
     }
 
 
-  public void add(Object newKey, Object newElement) throws InvalidObjectException {
-      // TODO: add code here
-      return null;
-  }
+    public void add(Object newKey, Object newElement) throws InvalidObjectException {
+        Item newItem = new Item(newKey, newElement);
+        if (size == btArray.length) {
+            expand(btArray.length*2);
+        }
+        ArrayPosition newAP = new ArrayPosition(size-1, newItem);
+        bubbleUp(newAP);
+        size++;
+    }
 
-  public Object removeRoot() throws EmptyHeapException {
-      // TODO: add code here
-      return null;
-  }
+    public Object removeRoot() throws EmptyHeapException {
+        if (isEmpty()) {
+            throw new EmptyHeapException("Error: Removing from empty heap");
+        }
+        ArrayPosition resultAP = (ArrayPosition) root();
+        ArrayPosition lastAP = btArray[size-1];
+        bubbleDown(lastAP);
+        size--;
+        Item result = (Item) resultAP.element();
+        return result;
+    }
 
+    private void bubbleDown(Position p) {
+        ArrayPosition curr = (ArrayPosition) p;
+        while (!isExternal(curr) && heapComp.isLessThan(getApKey(curr), getApKey(smallestChild(curr)))) {
+            swap(curr, smallestChild(curr));
+        }
+    }
+
+    private void bubbleUp(Position p) throws InvalidObjectException {
+        ArrayPosition curr = (ArrayPosition) p;
+        while (curr != root() && heapComp.isLessThan(getApKey(curr), getApKey(parent(curr)))) {
+            swap(curr, parent(curr));
+        }
+    }
+
+    private void expand(int s) {
+        ArrayPosition[] tempArray = new ArrayPosition[s];
+        for (int i = 0; i < btArray.length; i++) {
+            tempArray[i] = btArray[i];
+        }
+        btArray = tempArray;
+    }
+  
+    private void swap(Position p1, Position p2) {
+        ArrayPosition ap1 = (ArrayPosition) p1;
+        ArrayPosition ap2 = (ArrayPosition) p2;
+        int tempIndex = ap1.getIndex();
+        ap1.setIndex(ap2.getIndex());
+        ap2.setIndex(tempIndex);
+        btArray[ap1.getIndex()] = ap1;
+        btArray[ap2.getIndex()] = ap2;
+    }
+
+    private Object getApKey(Position p) {
+        ArrayPosition ap = (ArrayPosition) p;
+        Item temp = (Item) ap.element();
+        return temp.key();
+    }
+
+    private Position smallestChild(Position p) {
+        ArrayPosition curr = (ArrayPosition) p;
+        Position lc = leftChild(curr);
+        Position rc = rightChild(curr);
+        if (rc == null) {
+            return lc;
+        } else {
+            if (heapComp.isLessThan(getApKey(rc), getApKey(lc))) {
+                return rc;
+            } else {
+                return lc;
+            }
+        }
+    }
         // you may want to expand main; it is just provided as a sample
     public static void main (String[] args) {
 	    Comparator myComp = new IntegerComparator();
